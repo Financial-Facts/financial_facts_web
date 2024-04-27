13aafbcc44c25a862d09658bdc80a05753d6c4d1
Matthew Gabriel <gabrielmatthew20@gmail.com>
2024-04-27 12:24:07 -0500

    update footer

--- src/components/atoms/footer/Footer.tsx
+++ src/components/atoms/footer/Footer.tsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
 import "./Footer.scss"
 import { environment } from "../../../environment";
+import { messaging } from "../../../constants/messaging";
 
 function Footer() {
 
     return (
         <footer className="footer">
-            <i>2023 Financial Facts. All Rights Reserved.</i>
-            <span className="subtext">
-                Discounts calculated with data provided by <Link to={`${environment.fmpDocsUrl}`} target="_blank">Financial Modeling Prep</Link>
-            </span>
+            <div className="content-wrapper">
+                <div className="column disclaimer">
+                    <h2><u>DISCLAIMER</u></h2>
+                    <span className="subtext">
+                        { messaging.disclaimer }
+                    </span>
+                </div>
+                <div className="column information">
+                    <i>2023 Financial Facts. All Rights Reserved.</i>
+                    <span className="subtext">
+                        Discounts calculated with data provided by <Link to={`${environment.fmpDocsUrl}`} target="_blank">Financial Modeling Prep</Link>
+                    </span>
+                </div>
+            </div>
         </footer>
     )
   }
--- src/constants/messaging.ts
+++ src/constants/messaging.ts
@@ -1,6 +1,12 @@
 export const messaging = {
     aboutUs: `We are dedicated to helping investors make informed decisions by providing a comprehensive 
-    stock valuation service based on popular Rule #1 evaluation techniques. Our cloud-based infrastructure
-    scans the stock market for undervalued stocks, compiles them and delivers that information to empower
-    you in your investment journey. `
+        stock valuation service based on popular Rule #1 evaluation techniques. Our cloud-based infrastructure
+        scans the stock market for undervalued stocks, compiles them and delivers that information to empower
+        you in your investment journey. `,
+    disclaimer: `All opinions, evaluations, analyses, prices or other information or statements offered by this website are provided in the form of general information.
+        They do not constitute investment advice. Financial Facts and it's creators assume no liability for loss or damage, including, but not limited to, lost profits that may
+        result directly or indirectly from the use or reliance on the abovementioned opinions, evaluations, analyses, prices or other information offered by the website.
+        All the investment forms described here involve financial risk. The past performance of a security, an industry, a sector, a market, a financial product, a trading strategy
+        or the individual trade does not guarantee any future results or returns. As an investor, you yourself bear the full responsibility for your individual investment decisions.
+        Such decisions should be discussed in advance with your personal financial advisor in case of doubt.`
 }
